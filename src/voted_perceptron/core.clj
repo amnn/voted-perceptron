0f24be0cffc4d268173c43bf3b388a5ae2dd6dd5
Ashok Menon <amenon94@gmail.com>
2014-11-09 17:43:14 +0000

    Core: Adding methods to calculate error with various params.
    Also implemented cross-validation.

--- src/voted_perceptron/core.clj
+++ src/voted_perceptron/core.clj
@@ -1,7 +1,26 @@
 (ns voted-perceptron.core
-  (:gen-class))
+  (:require [voted-perceptron.learn :refer :all]
+            [voted-perceptron.sample :refer :all]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn- avg [xs] (/ (reduce + xs) (double (count xs))))
+
+(defn err
+  "Calculate the error for the voted perceptron algorithm learnt from `R`
+  repetitions of the `train`ing data, with `T` separate votes, and tested on
+  the `test`ing data."
+  [T R {:keys [test train]}]
+  (let [ws ((learn 256 T R) train)]
+    (avg (map (fn [[x y*]]
+                (if-not (= y* (classify ws x))
+                  1 0))
+              test))))
+
+(defn x-validate
+  "Given a sample, calculate the error when training on all but one sample and
+  then testing on the remaining sample, for every sample, and produce the
+  average of those errors"
+  [T R samples]
+  (let [cs (count samples)]
+    (->> (range cs)
+         (map #(err T R (pick-test-set % samples)))
+         avg)))
