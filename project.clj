8387366dce4936e3e36a0fc9be4b90716a9b35a9
Ashok Menon <amenon94@gmail.com>
2014-11-10 00:23:25 +0000

    Analysis: Adding error plots.

--- project.clj
+++ project.clj
@@ -3,7 +3,11 @@
 
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [amalloy/ring-buffer "1.0"]
+                 [incanter/incanter-charts "1.5.5"]
+                 [incanter-gorilla "0.1.0"]
                  [incanter/incanter-core "1.5.5"]]
+
+  :plugins [[lein-gorilla "0.3.3"]]
 
   :main ^:skip-aot voted-perceptron.core
   :target-path "target/%s"
